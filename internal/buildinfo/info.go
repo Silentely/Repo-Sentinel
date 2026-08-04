0489117acfdfb71b66fb06b33e37f85b96de59ec
Abner <22141172+Silentely@users.noreply.github.com>
2026-08-04 12:43:16 +0800

    🔥 chore: 移除无生产调用点的 ReadVersionFile 死代码

--- internal/buildinfo/info.go
+++ internal/buildinfo/info.go
@@ -1,14 +1,8 @@
 package buildinfo
 
 import (
-	"fmt"
-	"io"
-	"regexp"
 	"runtime"
-	"strings"
 )
-
-var versionPattern = regexp.MustCompile(`^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)$`)
 
 var (
 	version      = "dev"
@@ -38,19 +32,4 @@ func Current() Info {
 		BuildChannel: buildChannel,
 		GoVersion:    runtime.Version(),
 	}
-}
-
-// ReadVersionFile 读取、去除首尾空白并校验 X.Y.Z 格式的 SemVer。
-func ReadVersionFile(r io.Reader) (string, error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return "", fmt.Errorf("读取版本文件: %w", err)
-	}
-
-	value := strings.TrimSpace(string(content))
-	if !versionPattern.MatchString(value) {
-		return "", fmt.Errorf("版本 %q 不是有效的 X.Y.Z SemVer", value)
-	}
-
-	return value, nil
 }
